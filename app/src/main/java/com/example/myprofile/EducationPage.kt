853cf3414b437a6d5d85ef0d746b6d2f8ecaa0da
monukr26 <mkrtarwan11@gmail.com>
2025-05-18 08:37:58 +0530

    Education details uploaded

--- app/src/main/java/com/example/myprofile/EducationPage.kt
+++ app/src/main/java/com/example/myprofile/EducationPage.kt
@@ -1,6 +1,7 @@
 package com.example.myprofile
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,37 @@ class EducationPage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val result10th = findViewById<Button>(R.id.matric)
+        val result12th = findViewById<Button>(R.id.inter)
+        val resultBtech = findViewById<Button>(R.id.btech)
+
+        result10th.setOnClickListener {
+            val dailogView = layoutInflater.inflate(R.layout.matric, null)
+            val dailogBuilder = android.app.AlertDialog.Builder(this)
+            dailogBuilder.setView(dailogView)
+            dailogBuilder.setCancelable(true)
+            val dailog = dailogBuilder.create()
+            dailog.show()
+        }
+
+        result12th.setOnClickListener {
+            val dailogView = layoutInflater.inflate(R.layout.inter, null)
+            val dailogBuilder = android.app.AlertDialog.Builder(this)
+            dailogBuilder.setView(dailogView)
+            dailogBuilder.setCancelable(true)
+            val dailog = dailogBuilder.create()
+            dailog.show()
+        }
+
+        resultBtech.setOnClickListener {
+            val dailogView = layoutInflater.inflate(R.layout.btech, null)
+            val dailogBuilder = android.app.AlertDialog.Builder(this)
+            dailogBuilder.setView(dailogView)
+            dailogBuilder.setCancelable(true)
+            val dailog = dailogBuilder.create()
+            dailog.show()
         }
     }
 }
